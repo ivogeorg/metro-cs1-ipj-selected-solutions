0a6288a8e8fdfc0fd3076ec29fc6739838e4e6a2
Ivo Georgiev <ivogeorg@gmail.com>
2017-10-13 17:20:06 -0600

    added representative junit5 methods

--- homework-solutions/quadratic/test/edu/msud/cs/cs1/QuadraticTest.java
+++ homework-solutions/quadratic/test/edu/msud/cs/cs1/QuadraticTest.java
@@ -1,11 +1,36 @@
 package edu.msud.cs.cs1;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class QuadraticTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("This is beforeAll. Note that it is 'static'");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("This is afterAll. Note that it is 'static'");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("This is beforeEach");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("This is afterEach");
+    }
 
     @Test
     void test0() {
         System.out.println("This is test0");
+    }
+
+    @Test
+    void test1() {
+        System.out.println("This is test1");
     }
 }
