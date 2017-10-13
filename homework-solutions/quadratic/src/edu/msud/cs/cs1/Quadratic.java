1d24e939e8d1d53ff4fc357d19c235bf5991d44e
Ivo Georgiev <ivogeorg@gmail.com>
2017-10-13 16:18:43 -0600

    Inital commit

---
+++ homework-solutions/quadratic/src/edu/msud/cs/cs1/Quadratic.java
@@ -0,0 +1,15 @@
+package edu.msud.cs.cs1;
+
+public class Quadratic
+{
+    public static void main(String[] args)
+    {
+        // standard format of quadratic polynomial ax^2 + bx + c
+        double b = Double.parseDouble(args[0]);
+        double c = Double.parseDouble(args[1]);
+        double discriminant = b*b - 4.0*c;
+        double d = Math.sqrt(discriminant);
+        System.out.println((-b + d) / 2.0);
+        System.out.println((-b - d) / 2.0);
+    }
+}
