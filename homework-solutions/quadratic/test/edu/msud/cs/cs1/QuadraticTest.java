0da0d0d7e08a6b748026aef1bfd8ac023dccd779
Ivo Georgiev <ivogeorg@gmail.com>
2017-10-14 10:47:52 -0600

    added one passing junit 5 test

--- homework-solutions/quadratic/test/edu/msud/cs/cs1/QuadraticTest.java
+++ homework-solutions/quadratic/test/edu/msud/cs/cs1/QuadraticTest.java
@@ -1,36 +1,32 @@
 package edu.msud.cs.cs1;
 
+import edu.princeton.cs.introcs.Complex;
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class QuadraticTest {
 
+    private static double delta;
+
     @BeforeAll
-    static void beforeAll() {
-        System.out.println("This is beforeAll. Note that it is 'static'");
-    }
-
-    @AfterAll
-    static void afterAll() {
-        System.out.println("This is afterAll. Note that it is 'static'");
-    }
-
-    @BeforeEach
-    void beforeEach() {
-        System.out.println("This is beforeEach");
-    }
-
-    @AfterEach
-    void afterEach() {
-        System.out.println("This is afterEach");
-    }
+    static void beforeAll() { delta = 1e-10; }
 
     @Test
-    void test0() {
-        System.out.println("This is test0");
-    }
+    void test02() {
+        Quadratic q = new Quadratic(1, 1, 1);
+        Complex[] roots = q.roots();
 
-    @Test
-    void test1() {
-        System.out.println("This is test1");
+        // From Wolfram Alpha
+        // x≈-0.500000000000000 + 0.866025403784439 i
+        // x≈-0.500000000000000 - 0.866025403784439 i
+
+        assertEquals(roots[0].re(), -0.500000000000000, delta);
+        assertEquals(roots[0].im(), 0.866025403784439, delta);
+        assertEquals(roots[1].re(), -0.500000000000000, delta);
+        assertEquals(roots[1].im(), -0.866025403784439, delta);
+
+        assertTrue(q.isComplex());
+        assertFalse(q.isReal());
+        assertFalse(q.isSame());
     }
 }
