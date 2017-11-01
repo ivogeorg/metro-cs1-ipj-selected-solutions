056ee4f0388e81710b432d3accb0b9f80ba13ce6
Ivo Georgiev <ivogeorg@gmail.com>
2017-11-01 17:43:45 -0600

    Initial commit

--- homework-solutions/permutations/src/edu/msud/cs/cs1/Permutations.java
+++ homework-solutions/permutations/src/edu/msud/cs/cs1/Permutations.java
@@ -5,12 +5,12 @@ public class Permutations {
     private static final String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
     // TODO: any way to avoid the waste of String?
-    private static void printPermutation(String head, String tail) {
+    private void printPermutation(String head, String tail) {
         if (tail.length() == 0) {
             System.out.print(head + ' ');
         } else {
             for (int i = 0; i < tail.length(); i++) {
-                Permutations.printPermutation(head + tail.charAt(i),
+                printPermutation(head + tail.charAt(i),
                         (new StringBuffer(tail)).deleteCharAt(i).toString()); // TODO: ugly!
             }
         }
@@ -19,6 +19,8 @@ public class Permutations {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
 
-        printPermutation("", alphabet.substring(0, n));
+        Permutations perm = new Permutations();
+
+        perm.printPermutation("", alphabet.substring(0, n));
     }
 }
---
+++ homework-solutions/raw-picture/src/edu/msud/cs/cs1/RawPicture.java
@@ -0,0 +1,17 @@
+package edu.msud.cs.cs1;
+
+import java.awt.Color;
+
+public class RawPicture {
+    private int height, width;
+    private Color[] pixels;
+
+    RawPicture() {
+        height = width = 0;
+        pixels = null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
