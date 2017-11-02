5a129808fffe2ccdd9a1d4298fef911af1b142e9
Ivo Georgiev <ivogeorg@gmail.com>
2017-11-01 19:44:01 -0600

    Guidance: partial code and comments

--- homework-solutions/grayscale-histogram/README.md
+++ homework-solutions/grayscale-histogram/README.md
@@ -5,3 +5,4 @@ IPJ 3.1.4
 
 1. Use the `edu.princeton.cs.introcs.Picture` library to read in the image.
 2. Query the `Picture` object you created to count the occurrences of each grayscale shade in the image.
+3. Use the `edu.princeton.cs.introcs.Luminance` class to convert to grayscale.
--- homework-solutions/grayscale-histogram/src/edu/msud/cs/cs1/GrayscaleHistogram.java
+++ homework-solutions/grayscale-histogram/src/edu/msud/cs/cs1/GrayscaleHistogram.java
@@ -1,9 +1,40 @@
 package edu.msud.cs.cs1;
 
+import edu.princeton.cs.introcs.Luminance;
 import edu.princeton.cs.introcs.Picture;
 
+import java.awt.Color;
+
 public class GrayscaleHistogram {
-    GrayscaleHistogram() {}
+    private int[] grayscaleCounts;
+    // other data members to hold the histogram
+
+    public GrayscaleHistogram(String imgFilename) {
+        grayscaleCounts = new int[256]; // grayscale shades are from 0 (black) to 255 (white)
+
+        Picture picture = new Picture(imgFilename);
+        for (int col = 0; col < picture.width(); col++) {
+            for (int row = 0; row < picture.height(); row++) {
+                Color color = picture.get(col, row); // count each pixel of the image
+                Color gray = Luminance.toGray(color); // convert to grayscale (does nothing if already grayscale)
+
+                // Note: The gray Color contains three equal values (see Luminance.java), so you can get any one of them
+                grayscaleCounts[gray.getBlue()]++; // count the occurrence of the found value (in grayscale, R=G=B=Y, where Y is the monochrome luminance)
+            }
+        }
+
+        // TODO
+        // use grayscaleCounts to populate the histogram
+        constructHistogram();
+    }
+
+    private void constructHistogram() {
+        // TODO
+    }
+
+    public void printHistogram() {
+        // TODO
+    }
 
     public static void main(String[] args) {
 
