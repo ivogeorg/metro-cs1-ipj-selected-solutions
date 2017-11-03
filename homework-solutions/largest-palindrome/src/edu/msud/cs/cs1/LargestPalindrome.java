d5bf4a1b6449d7a5b623300c5c8e92f5115ac936
Ivo Georgiev <ivogeorg@gmail.com>
2017-11-03 11:39:48 -0600

    skeleton code

--- homework-solutions/largest-palindrome/src/edu/msud/cs/cs1/LargestPalindrome.java
+++ homework-solutions/largest-palindrome/src/edu/msud/cs/cs1/LargestPalindrome.java
@@ -1,4 +1,35 @@
 package edu.msud.cs.cs1;
 
 public class LargestPalindrome {
+
+    private String str, palindrome;
+
+    public LargestPalindrome(String input) {
+        str = input;
+        palindrome = longestPaliSub(str, str.length());
+    }
+
+    private boolean isPalindrome(String s) { // use to test the substrings
+        return s.equals((new StringBuffer(s)).reverse().toString());
+    }
+
+    private String longestPaliSub(String s, int length) {
+        // TODO
+        // Hint: Use ArrayList to collect the substrings of a particular length
+        // Hint: You terminate the recursion when you find a palindrome or length == 1
+        // Hint: Up to you whether to consider a single character a palindrome
+        // Note: There might be more than one palindrome of the same length (e.g. mama)
+
+        return "";
+    }
+
+    public String get() {
+        return palindrome;
+    }
+
+    public static void main(String[] args) {
+        System.out.println((new LargestPalindrome("alcohol")).get()); // should print "oho"
+        System.out.println((new LargestPalindrome("alhambra")).get()); // should print nothing
+        System.out.println((new LargestPalindrome("tattarrattat")).get()); // should print "tattarrattat"
+    }
 }
