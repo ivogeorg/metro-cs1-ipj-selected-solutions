54e90f807eddc32af9839571db50ee70d3718a7f
Ivo Georgiev <ivogeorg@gmail.com>
2017-11-12 12:16:48 -0700

    Functioning PeriodicTable

--- homework-solutions/periodic-table/src/edu/msud/cs/cs1/PeriodicTable.java
+++ homework-solutions/periodic-table/src/edu/msud/cs/cs1/PeriodicTable.java
@@ -2,6 +2,7 @@ package edu.msud.cs.cs1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.annotation.ElementType;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,10 +11,17 @@ public class PeriodicTable {
 
     private ArrayList<ChemicalElement> table;
 
+    // default constructor
     public PeriodicTable() {
         table = new ArrayList<>(NUMBER_OF_CHEMICAL_ELEMENTS); // allocate initial capacity
     }
 
+    // getters & setters
+    public ArrayList<ChemicalElement> getTable() {
+        return table;
+    }
+
+    // instance methods
     public boolean read(String filename) {
         boolean success = false;
         Scanner file = null;
@@ -30,25 +38,50 @@ public class PeriodicTable {
 
         if (!success) return success;
 
+        String line = "";
+        ChemicalElement element = null;
         try {
             while (file.hasNext()) {
-                String line = file.nextLine();
+                line = file.nextLine();
+
+                // DEBUGDEBUG
+                System.out.println(line);
+
                 Scanner elementData = new Scanner(line).useDelimiter("\\s*,\\s*");
-                ChemicalElement element = new ChemicalElement(); // element with nonsensical defaults
-                // read in data, parsing when necessary
+                element = new ChemicalElement(); // element with nonsensical defaults
+                table.add(element); // have to add it ahead of time
+                // read in data, parsing when necessary, and set the element's fields
                 if (elementData.hasNext()) element.setElementS(elementData.next()); else continue;
                 if (elementData.hasNext()) element.setNumberB(Byte.parseByte(elementData.next())); else continue;
                 if (elementData.hasNext()) element.setSymbolS(elementData.next()); else continue;
                 if (elementData.hasNext()) element.setWeightD(Double.parseDouble(elementData.next()));
-
-
+                if (elementData.hasNext()) element.setBoilD(Double.parseDouble(elementData.next()));
+                if (elementData.hasNext()) element.setMeltD(Double.parseDouble(elementData.next()));
+                if (elementData.hasNext()) element.setDensityI(Integer.parseInt(elementData.next()));
+                if (elementData.hasNext()) element.setVaporD(Double.parseDouble(elementData.next()));
+                if (elementData.hasNext()) element.setFusionD(Double.parseDouble(elementData.next()));
             }
-
         } catch (NumberFormatException nfe) {
-            System.err.println("Error: cannot parse invalid number");
+            System.err.println("Error: cannot parse invalid number on line \'" + line + "\'");
+            table.remove(element); // remove element since it might not be well-formed
             success = false;
         }
 
+        // cleanup
+        file.close();
+        file = null;
+
         return success;
+    }
+
+    public static void main(String[] args) {
+        PeriodicTable table = new PeriodicTable();
+
+        if (table.read(args[0])) {
+            System.out.println("Elements read: " + table.getTable().size());
+            for (ChemicalElement element: table.getTable()) System.out.println(element);
+        } else {
+            System.err.println("Problem reading file \'" + args[0] + "\'");
+        }
     }
 }
