75cbe10913857c21238fe4417ec3e21454df3ccf
Ivo Georgiev <ivogeorg@gmail.com>
2017-11-14 17:18:54 -0700

    took care of missing intermediate values fixed missing value in elements.csv molecularWeight finished, not tested

--- homework-solutions/periodic-table/src/edu/msud/cs/cs1/PeriodicTable.java
+++ homework-solutions/periodic-table/src/edu/msud/cs/cs1/PeriodicTable.java
@@ -2,7 +2,6 @@ package edu.msud.cs.cs1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.annotation.ElementType;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -22,7 +21,7 @@ public class PeriodicTable {
     }
 
     // instance methods
-    public boolean read(String filename) {
+    public boolean read(String filename, boolean hasHeader) {
         boolean success = false;
         Scanner file = null;
 
@@ -41,6 +40,9 @@ public class PeriodicTable {
         // declare and initialize variables that might be useful for handling exceptions (see catch block)
         String line = "";
         ChemicalElement element = null;
+
+        // check header
+        if (hasHeader) file.nextLine();
         try {
             while (file.hasNext()) {
                 line = file.nextLine();
@@ -50,15 +52,52 @@ public class PeriodicTable {
                 table.add(element); // have to add it ahead of time
 
                 // read in data, parsing when necessary, and set the element's fields
+                // the following are never missing
                 if (elementData.hasNext()) element.setElementS(elementData.next()); else continue;
                 if (elementData.hasNext()) element.setNumberB(Byte.parseByte(elementData.next())); else continue;
                 if (elementData.hasNext()) element.setSymbolS(elementData.next()); else continue;
                 if (elementData.hasNext()) element.setWeightD(Double.parseDouble(elementData.next())); else continue;
-                if (elementData.hasNext()) element.setBoilD(Double.parseDouble(elementData.next())); else continue;
-                if (elementData.hasNext()) element.setMeltD(Double.parseDouble(elementData.next())); else continue;
-                if (elementData.hasNext()) element.setDensityI(Integer.parseInt(elementData.next())); else continue;
-                if (elementData.hasNext()) element.setVaporD(Double.parseDouble(elementData.next())); else continue;
-                if (elementData.hasNext()) element.setFusionD(Double.parseDouble(elementData.next()));
+
+                // the following might be missing
+                if (elementData.hasNext()) {
+                    String data = elementData.next();
+                    if (!data.equals(""))
+                        element.setBoilD(Double.parseDouble(data));
+                    else
+                        element.setBoilD(ChemicalElement.MISSING_TEMPERATURE);
+                } else continue;
+
+                if (elementData.hasNext()) {
+                    String data = elementData.next();
+                    if (!data.equals(""))
+                        element.setMeltD(Double.parseDouble(data));
+                    else
+                        element.setMeltD(ChemicalElement.MISSING_TEMPERATURE);
+                } else continue;
+
+                if (elementData.hasNext()) {
+                    String data = elementData.next();
+                    if (!data.equals(""))
+                        element.setDensityI(Integer.parseInt(data));
+                    else
+                        element.setDensityI(ChemicalElement.MISSING_DENSITY);
+                } else continue;
+
+                if (elementData.hasNext()) {
+                    String data = elementData.next();
+                    if (!data.equals(""))
+                        element.setVaporD(Double.parseDouble(data));
+                    else
+                        element.setVaporD(ChemicalElement.MISSING_TEMPERATURE);
+                } else continue;
+
+                if (elementData.hasNext()) {
+                    String data = elementData.next();
+                    if (!data.equals(""))
+                        element.setFusionD(Double.parseDouble(data));
+                    else
+                        element.setFusionD(ChemicalElement.MISSING_TEMPERATURE);
+                }
             }
         } catch (NumberFormatException nfe) {
             System.err.println("Error: cannot parse invalid number on line \'" + line + "\'");
@@ -85,7 +124,6 @@ public class PeriodicTable {
         return element;
     }
 
-    // TODO finish...
     public double molecularWeight(String formula) {
         double weight = 0.0;
         StringBuffer symbol = null;
@@ -95,13 +133,30 @@ public class PeriodicTable {
             char c = formula.charAt(i);
             if (Character.isAlphabetic(c)) {
                 if (Character.isUpperCase(c)) {
-                    if (symbol != null) {
-//                        weight += table
+                    if (symbol != null) { // previous symbol
+                        try {
+                            weight += getElement(symbol.toString()).getWeightD() * numAtoms;
+                        } catch (UnknownChemicalElement uce) {
+                            System.err.println("ERROR: " + uce.getMessage());
+                            return 0.0;
+                        }
                     }
-                    symbol = new StringBuffer(c);
+                    symbol = new StringBuffer().append(c); // StringBuffer(c) interprets c as int for initial capacity
+                    numAtoms = 1;
+                } else {
+                    // assume symbol is not null and there is a capital letter in it already
+                    symbol.append(c); // Fun code inspection here :D
                 }
             } else if (Character.isDigit(c)) {
-
+                // assume symbol is not null and there is something in it
+                try {
+                    weight += getElement(symbol.toString()).getWeightD() * (int) c; // Fun code inspection here :D
+                } catch (UnknownChemicalElement uce) {
+                    System.err.println("ERROR: " + uce.getMessage());
+                    return 0.0;
+                }
+                symbol = null;
+                numAtoms = 1;
             } else {
                 System.err.println("ERROR: Unexpected character in formula \'"+c+"\'");
                 return 0.0;
@@ -114,11 +169,18 @@ public class PeriodicTable {
     public static void main(String[] args) {
         PeriodicTable table = new PeriodicTable();
 
-        if (table.read(args[0])) {
+        if (table.read(args[0], true)) {
             System.out.println("Elements read: " + table.getTable().size());
             for (ChemicalElement element: table.getTable()) System.out.println(element);
         } else {
             System.err.println("Problem reading file \'" + args[0] + "\'");
         }
+
+//        System.out.println("Molecular weight of H2O is " + table.molecularWeight("H2O"));
+
+        // test what next() does with a missing value
+//        Scanner test = new Scanner("Boron,5,B,10.81,2303.16,,2340,536.01,22.19,").useDelimiter(",");
+//        while (test.hasNext())
+//            System.out.println(test.next());
     }
 }
